b36ea857cac99a82db230fbb06ab89efb31e73c6
hydernew <hyder1890@live.com>
2018-09-19 19:33:35 +0530

    Adds punchcard component

---
+++ app/components/posts-timeline.js
@@ -0,0 +1,207 @@
+import Component from '@ember/component';
+import {
+  scalePoint,
+  scaleSqrt,
+  axisLeft,
+  axisBottom,
+  select
+} from "d3";
+export default Component.extend({
+  data: null,
+  didInsertElement() {
+    this._super(...arguments);
+    let id = this.elementId;
+    let data = this.data;
+    let labelsX = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23"
+    ];
+    let labelsY = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let margin = {
+      top: 20,
+      right: 30,
+      bottom: 30,
+      left: 120
+    };
+    let width = 900 - margin.left - margin.right;
+    let height = 350 - margin.top - margin.bottom;
+
+    let yScale = scalePoint().padding(0.5);
+    let xScale = scalePoint().padding(0.3);
+
+    yScale
+      .range([0, height])
+      .domain(labelsY)
+      .round(true);
+
+    xScale
+      .range([0, width])
+      .domain(labelsX);
+
+    let yAxis = axisLeft()
+      .scale(yScale);
+
+    let xAxis = axisBottom()
+      .tickFormat(function (d) {
+        return d;
+      })
+      .scale(xScale);
+
+    let svg = this.svg = select(`#${id}`).append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + [margin.left, margin.top] + ')');
+
+    svg.append("g")
+      .attr('class', 'y-axis')
+      .call(yAxis);
+
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .attr('class', 'x-axis')
+      .call(xAxis);
+
+    yScale.domain(data.map(function (d) {
+      return d.label;
+    }));
+
+    yAxis.scale(yScale);
+    svg.datum(data);
+
+    let rows = svg.selectAll("g.site")
+      .data(
+        function (d) {
+          return d;
+        },
+        function (d) {
+          return d.label;
+        }
+      );
+
+    rows.attr("class", "site")
+      .attr("transform", function (d) {
+        return "translate(0," + yScale(d.label) + ")";
+      });
+
+    rows.enter().append("g")
+      .attr("class", "site")
+      .attr("transform", function (d) {
+        return "translate(0," + yScale(d.label) + ")";
+      });
+    let radius = scaleSqrt();
+    let transition = svg.transition().duration(500);
+    let max = this.max;
+
+    radius
+      .range([0, 15])
+      .domain([0, max]);
+
+    xScale
+      .range([0, width])
+      .domain(labelsX);
+
+    yScale
+      .range([0, height])
+      .domain(labelsY);
+
+    svg.datum(data);
+
+    rows = svg.selectAll("g.site");
+    let circles = rows.selectAll("circle")
+      .data(
+        function (d) {
+          return d.values;
+        },
+        function (d, i) {
+          return i;
+        }
+      );
+
+    circles.exit()
+      .transition(transition)
+      .attr('r', 0)
+      .style("fill", "rgba(255,255,255,0)")
+      .remove();
+
+    circles
+      .attr("cy", 0)
+      .attr("cx", function (d, i) {
+        return xScale(i);
+      })
+      .transition(transition)
+      .attr("r", function (d) {
+        return radius(d);
+      });
+
+    circles.enter().append("circle")
+      .attr("cy", 0)
+      .attr("cx", function (d, i) {
+        return xScale(i);
+      })
+      .transition(transition)
+      .attr("r", function (d) {
+        return radius(d);
+      });
+
+    let tooltip = rows.selectAll('.tool-tip')
+      .data(
+        function (d) {
+          return d.values;
+        },
+        function (d, i) {
+          return i;
+        }
+      );
+
+    tooltip.enter().append('g')
+      .attr('class', 'tool-tip')
+      .on('mouseover', function () {
+        select(this).select("text").transition().duration(100).style('opacity', 1);
+      })
+      .on('mouseout', function () {
+        select(this).select("text").transition().style('opacity', 0);
+      });
+
+      tooltip = rows.selectAll('.tool-tip');
+      tooltip.append('text')
+      .style('text-anchor', 'middle')
+      .style('fill', '#ffffff')
+      .style('opacity', 0);
+
+      tooltip
+      .attr('transform', function (d, i) {
+        return "translate(" + xScale(i) + ",0)";
+      })
+      .select('text')
+      .text(function (d) {
+        return d;
+      })
+      .attr('y', 4)
+      .attr('x', 0);
+
+    tooltip.exit().remove();
+  }
+});
---
+++ tests/integration/components/posts-timeline-test.js
@@ -0,0 +1,230 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | posts-timeline', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    let data = [
+      {
+        "label": "Sunday",
+        "values": [
+          1,
+          1,
+          1,
+          1,
+          1,
+          0,
+          0,
+          0,
+          1,
+          1,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          2,
+          1,
+          2
+        ]
+      },
+      {
+        "label": "Monday",
+        "values": [
+          0,
+          1,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          1,
+          1,
+          1,
+          1,
+          2,
+          1,
+          0,
+          0,
+          3,
+          1,
+          3,
+          1,
+          0,
+          2,
+          0,
+          1
+        ]
+      },
+      {
+        "label": "Tuesday",
+        "values": [
+          0,
+          3,
+          1,
+          1,
+          0,
+          0,
+          0,
+          2,
+          0,
+          0,
+          0,
+          0,
+          2,
+          0,
+          0,
+          0,
+          0,
+          0,
+          2,
+          0,
+          1,
+          1,
+          3,
+          0
+        ]
+      },
+      {
+        "label": "Wednesday",
+        "values": [
+          4,
+          1,
+          0,
+          1,
+          0,
+          1,
+          2,
+          2,
+          0,
+          1,
+          1,
+          0,
+          1,
+          0,
+          1,
+          0,
+          1,
+          2,
+          0,
+          1,
+          0,
+          2,
+          0,
+          0
+        ]
+      },
+      {
+        "label": "Thursday",
+        "values": [
+          0,
+          0,
+          0,
+          0,
+          1,
+          0,
+          1,
+          1,
+          2,
+          2,
+          1,
+          0,
+          0,
+          2,
+          0,
+          0,
+          1,
+          2,
+          2,
+          1,
+          0,
+          2,
+          0,
+          1
+        ]
+      },
+      {
+        "label": "Friday",
+        "values": [
+          1,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          1,
+          2,
+          1,
+          0,
+          1,
+          2,
+          1,
+          3,
+          0,
+          0,
+          0,
+          0,
+          1,
+          0,
+          1,
+          2
+        ]
+      },
+      {
+        "label": "Saturday",
+        "values": [
+          1,
+          0,
+          1,
+          0,
+          0,
+          1,
+          0,
+          1,
+          0,
+          0,
+          1,
+          1,
+          1,
+          1,
+          1,
+          0,
+          1,
+          0,
+          0,
+          2,
+          0,
+          1,
+          1,
+          0
+        ]
+      }
+    ];
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    this.set('data', data);
+    await render(hbs`{{posts-timeline data=data max=4}}`);
+
+    assert.equal(this.element.querySelectorAll('circle')[5].getAttribute('r'), '0', 'Radius is greater than 0 when value is 0');
+
+    assert.equal(this.element.querySelector('circle').getAttribute('cx'), '9.533898305084733', 'X is calculated correctly for first element');
+
+    assert.equal(this.element.querySelector('g.y-axis').textContent.trim(), 'SundayMondayTuesdayWednesdayThursdayFridaySaturday', 'Y Axis is rendered correctly')
+    assert.equal(this.element.querySelector('g.x-axis').textContent.trim(), '01234567891011121314151617181920212223', 'X Axis is rendered correctly')
+
+    assert.equal(this.element.querySelector('g.tool-tip').textContent.trim(),'1', 'Tooltip is rendered correctly')
+  });
+});
