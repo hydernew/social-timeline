3169eea0525940ae231e3a1285fdacef7074a1bd
hydernew <hyder1890@live.com>
2018-09-25 00:13:20 +0530

    Classify posts based on the UTC day and hour from the timestamp

--- tests/unit/controllers/index-test.js
+++ tests/unit/controllers/index-test.js
@@ -9,4 +9,19 @@ module('Unit | Controller | index', function(hooks) {
     let controller = this.owner.lookup('controller:index');
     assert.ok(controller);
   });
+  test('chart data is processed correctly', function(assert) {
+    let controller = this.owner.lookup('controller:index');
+    let data = controller.data;
+    let json = {
+      timestamp: 1537130905
+    };
+    let sundayValues = data[0].values;
+    let mondayValues = data[1].values;
+    controller.processChartData(json);
+    let newSundayValues = data[0].values;
+    assert.notEqual(sundayValues, newSundayValues, 'New array instance is set for the given day');
+    assert.equal(mondayValues, data[1].values, 'Old array instance is maintained for an other day');
+    assert.equal(data, data, 'Same data object is maintained after processing');
+    assert.equal(newSundayValues[20], 1, 'Value is incremented for the hour in the timestamp');
+  });
 });
